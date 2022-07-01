8176498c76578d16c0115942af23e96268c7b37c
Klim Cheremnyh <63433482+Klim4ik20912@users.noreply.github.com>
2022-07-01 03:49:00 +0300

    update. add sleeping and changed the print v0.3
    1. was added cycle which sleeping main func every 5 sec.
    2. changed print to console
    3. some bug fix bruh...

--- vk_get_last_msg.py
+++ vk_get_last_msg.py
@@ -1,6 +1,7 @@
 import vk_api
 import time
 from datetime import datetime
+from time import sleep
 
 session = vk_api.VkApi(
 token='YOUR_TOKEN')
@@ -8,15 +9,17 @@ vk = session.get_api()
 
 
 def get_last_msg():
-    messages = session.method("messages.getHistory", {"user_id": '432682913', "offset": 0, "count": 1})
-    text = messages['items'][0]['text']
-    date = messages['items'][0]['date']
+    while True:
+        sleep(5)
+        messages = session.method("messages.getHistory", {"user_id": '432682913', "offset": 0, "count": 1})
+        text = messages['items'][0]['text']
+        date = messages['items'][0]['date']
 
-    read = convert_time(date)
+        read = convert_time(date)
 
-    final_time = count_stat(read)
+        final_time = count_stat(read)
 
-    print(f'msg_text: {text}. msg date: {read}. {final_time}')
+        print(f'текст сообщения: {text}. {final_time}')
 def convert_time(date):
     read_time = datetime.fromtimestamp(date)
     return read_time
@@ -25,10 +28,10 @@ def count_stat(vk_data):
     date_now = datetime.now()
     delta = date_now - vk_data
     if delta.seconds >= 3600:
-        return f'написал: {delta.seconds // 3600} часов назад'
+        return f'{delta.seconds // 3600} часов назад'
     elif delta.seconds >= 60:
-        return f'написал: {delta.seconds % 3600 // 60} минут назад'
+        return f'{delta.seconds % 3600 // 60} минут назад'
     else:
-        return f'написал: {delta.seconds} секунд назад'
+        return f'{delta.seconds} секунд назад'
 
 get_last_msg()
